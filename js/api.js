29a4f6c1331bc306680a4363ab971ec85b96dccb
Ruben Albertassi <albertassi88@gmail.com>
2026-03-16 14:38:45 -0300

    feat: adiciona função para buscar pokemons na pokeapi

---
+++ js/api.js
@@ -0,0 +1,10 @@
+const API_URL = "https://pokeapi.co/api/v2/pokemon";
+
+    export async function getPokemons(limit, offset) {
+
+    const response = await fetch(`${API_URL}?limit=${limit}&offset=${offset}`);
+    const data = await response.json();
+
+    return data.results;
+
+}

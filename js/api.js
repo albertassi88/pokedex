3138d107a63469c8eb1f14d4632066d1a5888408
Ruben Albertassi <albertassi88@gmail.com>
2026-03-16 15:07:05 -0300

    style: adiciona estilos iniciais da pokedex

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 const API_URL = "https://pokeapi.co/api/v2/pokemon";
 
-    export async function getPokemons(limit, offset) {
+export async function getPokemons(limit, offset) {
 
     const response = await fetch(`${API_URL}?limit=${limit}&offset=${offset}`);
     const data = await response.json();

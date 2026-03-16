bb2a2954f69f2bb2448b458ddb716db7065dde13
Ruben Albertassi <albertassi88@gmail.com>
2026-03-16 16:01:51 -0300

    feat(main): integra api com renderização dos pokemons

--- js/api.js
+++ js/api.js
@@ -1,10 +1,13 @@
 const API_URL = "https://pokeapi.co/api/v2/pokemon";
 
 export async function getPokemons(limit, offset) {
+  const response = await fetch(`${API_URL}?limit=${limit}&offset=${offset}`);
+  const data = await response.json();
+  return data.results;
+}
 
-    const response = await fetch(`${API_URL}?limit=${limit}&offset=${offset}`);
-    const data = await response.json();
-
-    return data.results;
-
+export async function getPokemonDetails(url){
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
---
+++ js/main.js
@@ -0,0 +1,23 @@
+import { getPokemons, getPokemonDetails } from "./api.js";
+import { renderPokemon } from "./render.js";
+
+let page = 1;
+const limit = 18;
+
+async function loadPokemons(){
+
+  const offset = (page - 1) * limit;
+
+  const pokemons = await getPokemons(limit, offset);
+
+  const container = document.getElementById("pokemon-list");
+  container.innerHTML = "";
+
+  for(const pokemon of pokemons){
+    const details = await getPokemonDetails(pokemon.url);
+    renderPokemon(details);
+  }
+
+}
+
+loadPokemons();

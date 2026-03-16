d92012821aacc7ba511be74617235a8a41ad937e
Ruben Albertassi <albertassi88@gmail.com>
2026-03-16 15:15:05 -0300

    feat: implementa renderização de cards de pokemon no render.js

--- js/render.js
+++ js/render.js
@@ -1,1 +1,18 @@
 
+export function renderPokemon(pokemon){
+
+  const container = document.getElementById("pokemon-list");
+
+  const card = document.createElement("div");
+  card.classList.add("pokemon-card");
+
+  card.innerHTML = `
+    <div class="type">${pokemon.types[0].type.name}</div>
+    <div class="number">#${pokemon.id}</div>
+    <img src="${pokemon.sprites.other['official-artwork'].front_default}" />
+    <h3>${pokemon.name}</h3>
+  `;
+
+  container.appendChild(card);
+
+}
